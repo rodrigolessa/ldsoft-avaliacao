e3b170ffe9133b6595e2fd12e7ce41f3e911bfb6
Rodrigo <rodrigolsr@gmail.com>
2016-07-26 22:53:07 -0300

    projeto inicial

---
+++ LDSoft.Avaliacao.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using console1 = System.Console;
+
+namespace LdSoft.Avaliacao.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            console1.ReadKey();
+        }
+    }
+}
---
+++ LDSoft.Avaliacao.UI.Web/app/app.config.js
@@ -0,0 +1,8 @@
+﻿angular.module('ldsoft.avaliacao')
+    .config([
+        '$routeProvider', function ($routeProvider) {
+            $routeProvider.otherwise({
+                redirectTo: '/'
+            });
+        }
+    ]);
